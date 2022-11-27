3497e9609a0b3a5ed87933c25af3392acf3eb9eb
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2022-11-27 19:13:44 +0300

    reformatting

--- src/gazeboChangeStart.cpp
+++ src/gazeboChangeStart.cpp
@@ -4,9 +4,11 @@
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "gazebo_change_start");
+    ros::init(argc, argv, "gazeboChangeStart");
+
     ros::NodeHandle nh;
     ros::ServiceClient client = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+
     gazebo_msgs::SetModelState srv;
     srv.request.model_state.model_name = "turtlebot3_burger";
     srv.request.model_state.pose.position.x = 2;
@@ -17,5 +19,6 @@ int main(int argc, char **argv)
     srv.request.model_state.pose.orientation.z = 0;
     srv.request.model_state.pose.orientation.w = 1;
     client.call(srv);
+
     return 0;
 }
