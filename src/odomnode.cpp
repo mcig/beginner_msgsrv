202cee3c95b9ef65980bc8d0558fd9a14d982089
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2022-12-12 09:52:00 +0300

    add rotate

--- src/odomnode.cpp
+++ src/odomnode.cpp
@@ -8,6 +8,7 @@
 ros::Publisher pub;
 geometry_msgs::Twist msg;
 geometry_msgs::Pose2D current_pose;
+void coolRotate(ros::NodeHandle, double);
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
 {
@@ -51,8 +52,52 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "odomnode");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("/odom", 1000, odomCallback);
-    move(n);
+    coolRotate(n, 90);
+    coolRotate(n, 60);
+    coolRotate(n, 120);
     ros::spin();
 
     return 0;
+}
+
+void coolRotate(ros::NodeHandle n, double degrees)
+{
+    pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+    ros::Rate loop_rate(10);
+
+    msg.linear.x = 0;
+    msg.angular.z = 0.2;
+
+    double targetrad = degrees * M_PI / 180.0;
+    double start_theta = current_pose.theta;
+    double c = 0.5;
+
+    while (ros::ok())
+    {
+        std::cout << "Target: " << msg.angular.z << std::endl;
+        msg.linear.x = 0;
+        msg.angular.z = c * (targetrad - current_pose.theta);
+
+        // handle negative radians using M_PI
+        if (msg.angular.z < 0)
+        {
+            // calculate using pi
+            msg.angular.z = c * (targetrad - current_pose.theta - 2 * M_PI);
+            std::cout << "CALC: " << msg.angular.z << std::endl;
+        }
+
+        if (targetrad - start_theta > 0 && msg.angular.z < 0.008)
+            break;
+        else if (targetrad - start_theta < 0 && msg.angular.z > 0.008)
+            break;
+
+        pub.publish(msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    msg.linear.x = 0;
+    msg.angular.z = 0;
+    pub.publish(msg);
+    ROS_INFO("Rotated %f degrees", degrees);
 }
